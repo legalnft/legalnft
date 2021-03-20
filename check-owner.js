fad77f4b8b9e429e9338e2a5da14f339b064a2a6
smartsas <58598126+smartsas@users.noreply.github.com>
2021-03-20 12:56:14 +0100

    presentation

--- check-owner.js
+++ check-owner.js
@@ -1,7 +1,7 @@
 
-const Web3 = require('web3');
+//const Web3 = require('web3');
 
-const web3 = new Web3(Web3.givenProvider||'https://ropsten.infura.io/v3/d3e5e3e6c2e84b5db33392f297d77c4d');
+//const web3 = new Web3(Web3.givenProvider||'https://ropsten.infura.io/v3/d3e5e3e6c2e84b5db33392f297d77c4d');
 
 let accounts = [];
 
@@ -21,13 +21,24 @@ document.getElementById("find").addEventListener("click", function (e) {
 
 	
 	
-	const fetchPromise = fetch('https://api.opensea.io/api/v1/asset/' + nftAddress + '/' + tokenId, options);
+	//const fetchPromise = fetch('https://api.opensea.io/api/v1/asset/' + nftAddress + '/' + tokenId, options);
 	
 	
 		fetchPromise.then(response => {
 		  return response.json();
 		}).then(result => {
-		  console.log(result.creator.address);
+			
+			if (result.creator.address == compteEthereum) {
+				
+				
+				
+			} else {
+				
+				document.getElementById("find.pres").style.display = 'none';
+				document.getElementById("protect").style.display = 'block';
+				
+			} 
+			
 		});
   
 	
