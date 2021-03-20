04c1acc7d19aa469efb1c6c44ba9fe7aef0c5660
smartsas <58598126+smartsas@users.noreply.github.com>
2021-03-20 20:23:09 +0100

    check license

--- dirty-code.js
+++ dirty-code.js
@@ -521,3 +521,29 @@ document.getElementById("submit").addEventListener("click", function() {
 
 
 })
+
+
+
+
+document.getElementById("Find").addEventListener("click", function (){
+	
+	
+	const nft_location = document.getElementById ("nft-loc");
+	const id_nft = document.getElementById("id-nft");
+	
+	
+	contract.methods.getLicense(nft_location, id_nft ).call({from: accounts[0]})
+	
+	.then(function(result){
+	
+		
+		const iframe = document.createElement("iframe");
+		
+		iframe.src = result;
+		
+		document.getElementById("check-license").replaceChild(iframe, document.getElementById("z"));
+		
+});
+
+	
+})
