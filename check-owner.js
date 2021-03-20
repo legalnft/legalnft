f8febc20b400274bc311e5d9537818e68f499a96
smartsas <58598126+smartsas@users.noreply.github.com>
2021-03-20 09:57:25 +0100

    reorg 2

--- check-owner.js
+++ check-owner.js
@@ -1,35 +1,35 @@
-const options = {method: 'GET'};
 
+const Web3 = require('web3');
 
+const web3 = new Web3(Web3.givenProvider||'https://ropsten.infura.io/v3/d3e5e3e6c2e84b5db33392f297d77c4d');
 
+let accounts = [];
 
 
 
 document.getElementById("find").addEventListener("click", function (e) {
 	
-		
+	const compteEthereum = accounts[0];
+	
+	console.log(compteEthereum);
+	
 	const nftAddress = document.getElementById("nft-address").value;
 	
 	const tokenId = document.getElementById("token-id").value;
 	
+	const options = {method: 'GET'};
 
-	fetch('https://api.opensea.io/api/v1/asset/' + nftAddress + '/' + tokenId, options)
 	
 	
+	const fetchPromise = fetch('https://api.opensea.io/api/v1/asset/' + nftAddress + '/' + tokenId, options);
 	
 	
-
-	  .then(response => console.log(response.json()))
-	  .catch(err => console.error(err));
+		fetchPromise.then(response => {
+		  return response.json();
+		}).then(result => {
+		  console.log(result.creator.address);
+		});
   
-  
-  
-	
-	
-	
-
-
-	
 	
 })
 
