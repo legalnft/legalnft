72a68b6b81c73103fc0a987577ef693b870a88ad
smartsas <58598126+smartsas@users.noreply.github.com>
2021-03-20 15:43:08 +0100

    update n

--- iframe.js
+++ iframe.js
@@ -12,17 +12,10 @@ document.getElementById("Find").addEventListener("click", function (){
 		
 		const iframe = document.createElement("iframe");
 		
+		iframe.src = result;
 		
+		document.getElementById("check-license").replaceChild(iframe, document.getElementById("z"));
 		
-		document.body.replaceChild(iframe, document.getElementById("check-license");
-		
-		listItem.parentNode.replaceChild(newItem, listItem);
-
-
-
-    <iframe src="/uploads/media/default/0001/01/540cb75550adf33f281f29132dddd14fded85bfc.pdf" width="100%" height="500px">
-
-    ...
 });
 
 	
