8fcbbda0b149e3a7d00172312fc82a494d37221b
smartsas <58598126+smartsas@users.noreply.github.com>
2021-03-20 20:33:30 +0100

    metamask again :))))))))))))))))

--- dirty-code.js
+++ dirty-code.js
@@ -528,11 +528,11 @@ document.getElementById("submit").addEventListener("click", function() {
 document.getElementById("Find").addEventListener("click", function (){
 	
 	
-	const nft_location = document.getElementById ("nft-loc");
+	const nft_location = document.getElementById("nft-loc");
 	const id_nft = document.getElementById("id-nft");
 	
 	
-	contract.methods.getLicense(nft_location, id_nft ).call({from: accounts[0]})
+	contract.methods.getLicense(nft_location, id_nft ).call({from: ethereumAccount})
 	
 	.then(function(result){
 	
