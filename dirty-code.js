9a24834a7e750fed7f612bd3a076456cbc4aff20
smartsas <58598126+smartsas@users.noreply.github.com>
2021-03-20 18:00:20 +0100

    prompt meta

--- dirty-code.js
+++ dirty-code.js
@@ -17,11 +17,15 @@ const web3 = new Web3(Web3.givenProvider||'https://ropsten.infura.io/v3/d3e5e3e6
 
 let accounts = [];
 
-accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-		
-let ethereumAccount = accounts[0];
+async function promptMeta (){
+	
+	accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+			
+	let ethereumAccount = accounts[0];
 
+}
 
+promptMeta();
 
 const abi = [
 	{
