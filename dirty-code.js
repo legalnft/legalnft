1ed19121c6565ffb0149994ea85d0d68c57076a6
smartsas <58598126+smartsas@users.noreply.github.com>
2021-03-20 16:45:24 +0100

    metamask2

--- dirty-code.js
+++ dirty-code.js
@@ -17,7 +17,19 @@ const web3 = new Web3(Web3.givenProvider||'https://ropsten.infura.io/v3/d3e5e3e6
 
 let accounts = [];
 
-
+async function detectMetamask(){
+	
+	const provider = await detectEthereumProvider()
+	
+	if (provider) {
+    
+		console.log("c bon");
+  } else {
+		
+		alert("You need metamask to use this dapp");	
+  }
+		
+}
 
 
 
