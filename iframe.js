014bf7f9d909f02a5291d06b88f716f96fadd58a
smartsas <58598126+smartsas@users.noreply.github.com>
2021-03-20 15:37:10 +0100

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -8,6 +8,20 @@ document.getElementById("Find").addEventListener("click", function (){
 	contract.methods.getLicense(nft_location, id_nft ).call({from: accounts[0]})
 	
 	.then(function(result){
+	
+		
+		const iframe = document.createElement("iframe");
+		
+		
+		
+		document.body.replaceChild(iframe, document.getElementById("check-license");
+		
+		listItem.parentNode.replaceChild(newItem, listItem);
+
+
+
+    <iframe src="/uploads/media/default/0001/01/540cb75550adf33f281f29132dddd14fded85bfc.pdf" width="100%" height="500px">
+
     ...
 });
 
